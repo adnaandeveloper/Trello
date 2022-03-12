0d331c283358b533dd1c06b913518c6fc54221e6
amaydeveloper <amaydeveloper@MacBook-Pro-tilhrende-amaydeveloper.local>
2022-03-12 18:19:08 +0100

    added storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Board from './screens/Board'
-import Boards from './screens/Boards'
-import Signup from './screens/Signup'
-import User from './screens/User'
-import Login from './screens/Login'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Board from './screens/Board';
+import Boards from './screens/Boards';
+import Signup from './screens/Signup';
+import User from './screens/User';
+import Login from './screens/Login';
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="login" element={<Login />} />
-                <Route path="Boards" element={<Boards />} />
-                <Route path="signup" element={<Signup />} />
-                <Route path="board/:id" element={<Board />} />
-                <Route path="user/:id" element={<User />} />
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='login' element={<Login />} />
+        <Route path='Boards' element={<Boards />} />
+        <Route path='signup' element={<Signup />} />
+        <Route path='board/:id' element={<Board />} />
+        <Route path='user/:id' element={<User />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
