9add75873a97ca3141dc4d7916b7bca07aa09cce
amaydeveloper <amaydeveloper@MacBook-Pro-tilhrende-amaydeveloper.local>
2022-04-21 15:50:53 +0200

    fixed board and added  ClickAwayListener

--- src/modules/common/components/AddTaskListField.tsx
+++ src/modules/common/components/AddTaskListField.tsx
@@ -1,6 +1,9 @@
 import { Grid, TextField } from '@mui/material';
 import AddTaskListFieldButton from './AddTaskListFieldButton';
 import CloseIcon from '@mui/icons-material/Close';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
 
 type Props = {
   onClickHandler: () => void;
@@ -10,54 +13,55 @@ type Props = {
 };
 const AddTaskListField = (props: Props) => {
   return (
-    <Grid container sx={{ marginTop: '5.5rem', height: 60 }}>
-      <Grid
-        item
-        sx={{ backgroundColor: '#ebecf0', padding: '1px', marginTop: '5' }}
-      >
-        <TextField
-          value={props.authorName}
-          onChange={(e) => props.onChangHanlerAddAuthor(e.target.value)}
-          size='small'
-          variant='outlined'
-          placeholder='Enter list Litle...'
-          sx={{
-            width: '276px',
-            justifyContent: 'flex-start',
-            color: 'white',
-            textTransform: 'none',
-            backgroundColor: 'white',
-            padding: '0',
-            '& .MuiOutlinedInput-notchedOutline legend': { display: 'none' },
-            '& .MuiInputBase-root': { marginTop: '3px' },
-            m: 1,
-          }}
-        ></TextField>
-        <Grid container direction='row' spacing={1}>
-          <Grid item>
-            {' '}
-            <AddTaskListFieldButton
-              onClickHandler={props.onClickHandler}
-              AddNewAotuhersToTheAuthors={props.AddNewAotuhersToTheAuthors}
-            />
-          </Grid>
-          <Grid item>
-            <CloseIcon
-              onClick={() => props.onClickHandler()}
-              sx={{
-                fontSize: '25px',
-                marginTop: '10px',
-                color: '#42526e',
-                '&:hover': {
-                  color: '#172b4d',
-                  cursor: 'pointer',
-                },
-              }}
-            />
+    <ClickAwayListener onClickAway={() => props.onClickHandler()}>
+      <Grid container sx={{ marginTop: '5.5rem', height: 60 }}>
+        <Grid
+          item
+          sx={{ backgroundColor: '#ebecf0', padding: '1px', marginTop: '5' }}
+        >
+          <TextField
+            value={props.authorName}
+            onChange={(e) => props.onChangHanlerAddAuthor(e.target.value)}
+            size='small'
+            variant='outlined'
+            placeholder='Enter list Litle...'
+            sx={{
+              width: '276px',
+              justifyContent: 'flex-start',
+              color: 'white',
+              textTransform: 'none',
+              backgroundColor: 'white',
+              padding: '0',
+              '& .MuiOutlinedInput-notchedOutline legend': { display: 'none' },
+              '& .MuiInputBase-root': { marginTop: '3px' },
+              m: 1,
+            }}
+          ></TextField>
+          <Grid container direction='row' spacing={1}>
+            <Grid item>
+              <AddTaskListFieldButton
+                onClickHandler={props.onClickHandler}
+                AddNewAotuhersToTheAuthors={props.AddNewAotuhersToTheAuthors}
+              />
+            </Grid>
+            <Grid item>
+              <CloseIcon
+                onClick={() => props.onClickHandler()}
+                sx={{
+                  fontSize: '25px',
+                  marginTop: '10px',
+                  color: '#42526e',
+                  '&:hover': {
+                    color: '#172b4d',
+                    cursor: 'pointer',
+                  },
+                }}
+              />
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </ClickAwayListener>
   );
 };
 
--- src/modules/dashboard/components/AddCardTextFeild.tsx
+++ src/modules/dashboard/components/AddCardTextFeild.tsx
@@ -2,6 +2,7 @@ import { Grid, TextField } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 
 import AddCardButton from './AddCardButton';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
 
 type Props = {
   togleShowTextField: () => void;
@@ -12,67 +13,69 @@ type Props = {
 };
 const AddCardTextFeild = (props: Props) => {
   return (
-    <Grid container sx={{}}>
-      <Grid
-        item
-        sx={{
-          backgroundColor: '#ebecf0',
-          padding: '1px',
-          height: '114px',
-          marginLeft: '-8px',
-          width: '250px',
-        }}
-      >
-        <TextField
-          id='outlined-basic'
-          variant='outlined'
-          fullWidth
-          multiline
-          onChange={(e) => props.addQuoteName(e.target.value)}
-          placeholder='Enter a title for this card...'
+    <ClickAwayListener onClickAway={() => props.togleShowTextField()}>
+      <Grid container sx={{}}>
+        <Grid
+          item
           sx={{
-            m: 1,
-
-            width: '225px',
-            justifyContent: 'flex-start',
-            color: 'white',
-            textTransform: 'none',
-            backgroundColor: 'white',
-            padding: '0',
-            height: '58px',
-            '& .MuiOutlinedInput-notchedOutline legend': {
-              display: 'none',
-            },
-            '& .MuiInputBase-root': { marginTop: '3px' },
+            backgroundColor: '#ebecf0',
+            padding: '1px',
+            height: '114px',
+            marginLeft: '-8px',
+            width: '250px',
           }}
-        ></TextField>
-        <Grid container direction='row' spacing={1}>
-          <Grid item sx={{ marginLeft: '17px' }}>
-            {' '}
-            <AddCardButton
-              togleShowTextField={props.togleShowTextField}
-              addQuote={props.addQuote}
-              listId={props.listId}
-              sendTheTitle={props.sendTheTitle}
-            />
-          </Grid>
-          <Grid item>
-            <CloseIcon
-              onClick={() => props.togleShowTextField()}
-              sx={{
-                fontSize: '25px',
-                marginTop: '4px',
-                color: '#42526e',
-                '&:hover': {
-                  color: '#172b4d',
-                  cursor: 'pointer',
-                },
-              }}
-            />
+        >
+          <TextField
+            id='outlined-basic'
+            variant='outlined'
+            fullWidth
+            multiline
+            onChange={(e) => props.addQuoteName(e.target.value)}
+            placeholder='Enter a title for this card...'
+            sx={{
+              m: 1,
+
+              width: '225px',
+              justifyContent: 'flex-start',
+              color: 'white',
+              textTransform: 'none',
+              backgroundColor: 'white',
+              padding: '0',
+              height: '58px',
+              '& .MuiOutlinedInput-notchedOutline legend': {
+                display: 'none',
+              },
+              '& .MuiInputBase-root': { marginTop: '3px' },
+            }}
+          ></TextField>
+          <Grid container direction='row' spacing={1}>
+            <Grid item sx={{ marginLeft: '17px' }}>
+              {' '}
+              <AddCardButton
+                togleShowTextField={props.togleShowTextField}
+                addQuote={props.addQuote}
+                listId={props.listId}
+                sendTheTitle={props.sendTheTitle}
+              />
+            </Grid>
+            <Grid item>
+              <CloseIcon
+                onClick={() => props.togleShowTextField()}
+                sx={{
+                  fontSize: '25px',
+                  marginTop: '4px',
+                  color: '#42526e',
+                  '&:hover': {
+                    color: '#172b4d',
+                    cursor: 'pointer',
+                  },
+                }}
+              />
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </ClickAwayListener>
   );
 };
 
